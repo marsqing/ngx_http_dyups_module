f471a4f43493ef97019294d678708a2752e603da
marsqing <qing.gu@dianping.com>
2013-11-07 11:43:13 +0800

    refactor out .h to be reuse by other modules

--- ngx_http_dyups_module.c
+++ ngx_http_dyups_module.c
@@ -1,6 +1,7 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
+#include "ngx_http_dyups_module.h"
 
 
 #define NGX_DYUPS_DELETING     1
@@ -13,30 +14,6 @@
 
 #define ngx_dyups_add_timer(ev, timeout)                                      \
     if (!ngx_exiting && !ngx_quit) ngx_add_timer(ev, (timeout))
-
-
-typedef struct {
-    ngx_uint_t                     idx;
-    ngx_uint_t                    *count;
-    ngx_uint_t                     deleted;
-    ngx_flag_t                     dynamic;
-    ngx_pool_t                    *pool;
-    ngx_http_conf_ctx_t           *ctx;
-    ngx_http_upstream_srv_conf_t  *upstream;
-} ngx_http_dyups_srv_conf_t;
-
-
-typedef struct {
-    ngx_flag_t                     enable;
-    ngx_flag_t                     trylock;
-    ngx_array_t                    dy_upstreams;/* ngx_http_dyups_srv_conf_t */
-    ngx_str_t                      conf_path;
-    ngx_str_t                      shm_name;
-    ngx_uint_t                     shm_size;
-    ngx_msec_t                     read_msg_timeout;
-    ngx_resolver_t                *resolver;
-    ngx_msec_t                     resolver_timeout;
-} ngx_http_dyups_main_conf_t;
 
 
 typedef struct {
